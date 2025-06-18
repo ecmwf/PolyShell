948d180614ef9d00137de89e470677a986f67b6d
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-06-18 22:26:16 +0100

    feat: add plotting script

---
+++ scripts/plot_poly.py
@@ -0,0 +1,23 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from polyshell import reduce_polygon
+from polyshell.geometry import Coord, Polygon
+
+if __name__ == "__main__":
+    with open("./tests/data/ionian_polygon_points.pkl", "rb") as f:
+        original_polygon = np.array(pickle.load(f))
+
+    original_polygon = Polygon([Coord(tuple(point)) for point in original_polygon])
+    reduced_polygon = reduce_polygon(original_polygon, epsilon=1e-6)
+
+    # Extract data
+    x_orig, y_orig = zip(*original_polygon)
+    x_reduced, y_reduced = zip(*reduced_polygon)
+
+    # Plot original and reduced polygons
+    plt.plot(x_orig, y_orig, "b-")
+    plt.plot(x_reduced, y_reduced, "r-")
+    plt.show()
