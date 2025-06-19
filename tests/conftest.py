5d581ee81a2c234794a267a4b41b01635f4cd782
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-06-19 12:13:16 +0100

    test: add random polygon fixture
    Co-authored-by: Jo Wayne Tan <waynetan9994@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 """Fixtures for testing."""
 
 import pickle
+import random
 
 import pytest
+from polygenerator import random_polygon as _random_polygon  # type: ignore
 from polyshell.geometry import Polygon
 
 
@@ -11,3 +13,12 @@ def ionian_sea() -> Polygon:
     """Polygon generated from the Ionian Sea."""
     with open("tests/data/ionian_polygon_points.pkl", "rb") as f:
         return Polygon.from_array(pickle.load(f))
+
+
+@pytest.fixture(params=[(1023, 0)])
+def random_polygon(request: pytest.FixtureRequest) -> Polygon:
+    """Generate a random polygon with given seed and number of points."""
+    num_points, seed = request.param
+    random.seed(seed)
+    poly: list[tuple[float, float]] = _random_polygon(num_points)
+    return Polygon.from_array(list(reversed(poly)))
