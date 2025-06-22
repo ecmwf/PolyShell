228703eb18adf15de9476b5670c8b5fb746bcb4b
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-06-22 12:03:30 +0100

    feat: shard rtree across workers

--- src/polyshell/reducer.py
+++ src/polyshell/reducer.py
@@ -1,7 +1,6 @@
 import heapq
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass, field
-from threading import Lock
 from typing import Iterable
 
 from rtree.index import Rtree
@@ -31,9 +30,9 @@ def data_stream(geoms: Iterable[Geometry]):
         yield (ID, item.bbox(), item)
 
 
-def worker_wraps(epsilon: float, tree: Rtree, mutex: Lock):
+def worker_wraps(epsilon: float):
     def worker(ls: LineString) -> LineString:
-        return vw_preserve(ls, epsilon, tree, mutex)
+        return vw_preserve(ls, epsilon)
 
     return worker
 
@@ -42,9 +41,6 @@ def reduce_polygon(
     polygon_points: Polygon, epsilon: float, max_workers: int
 ) -> Polygon:
     """Reduce a polygon while retaining coverage."""
-    tree = Rtree(data_stream(polygon_points.lines()))
-    mutex = Lock()  # Rtree is not thread-safe, lock with a mutex
-
     # Slice into LineStrings
     vertices = ConvexHull(polygon_points.to_array()).vertices[::-1]
     vertices = list([*vertices, vertices[0]])
@@ -55,21 +51,21 @@ def reduce_polygon(
 
     # Dispatch and reconstitute
     with ThreadPoolExecutor(max_workers) as tpe:
-        reduced_segments = tpe.map(worker_wraps(epsilon, tree, mutex), segments)
+        reduced_segments = tpe.map(worker_wraps(epsilon), segments)
 
     return Polygon.merge(reduced_segments)
 
 
-def vw_preserve(
-    polygon_points: LineString, epsilon: float, tree: Rtree, mutex: Lock
-) -> LineString:
+def vw_preserve(polygon_points: LineString, epsilon: float) -> LineString:
     """Visvalingam-Whyatt line reduction algorithm adapted to prevent crossings."""
     if len(polygon_points) < 3 or epsilon <= 0:
         return polygon_points
 
     max_points = len(polygon_points)
 
-    adjacent = [(i - 1, i + 1) for i in range(len(polygon_points))]
+    tree = Rtree(data_stream(polygon_points.lines()))
+
+    adjacent = [(i - 1, i + 1) for i in range(max_points)]
 
     pq = [
         VWScore(
@@ -96,7 +92,7 @@ def vw_preserve(
             continue
 
         # Check for self-intersection
-        if tree_intersect(tree, smallest, polygon_points, mutex):
+        if tree_intersect(tree, smallest, polygon_points):
             # Skip this point
             continue
 
@@ -114,14 +110,12 @@ def vw_preserve(
 
         line_1 = Line((left_point, middle_point))
         line_2 = Line((middle_point, right_point))
-        with mutex:
-            tree.delete(ID, line_1.bbox())
-            tree.delete(ID, line_2.bbox())
+        tree.delete(ID, line_1.bbox())
+        tree.delete(ID, line_2.bbox())
 
         # Reconnect vertices
         new_line = Line((left_point, right_point))
-        with mutex:
-            tree.insert(ID, new_line.bbox(), new_line)
+        tree.insert(ID, new_line.bbox(), new_line)
 
         # Update adjacent triangles
         recompute_triangles(polygon_points, pq, ll, left, right, rr, max_points)
@@ -159,9 +153,7 @@ def recompute_triangles(
         heapq.heappush(pq, v)
 
 
-def tree_intersect(
-    tree: Rtree, triangle: VWScore, points: LineString, mutex: Lock
-) -> bool:
+def tree_intersect(tree: Rtree, triangle: VWScore, points: LineString) -> bool:
     """Check if removal of a triangle causes a self-intersection."""
     new_segment_start = points[triangle.left]
     new_segment_end = points[triangle.right]
@@ -172,8 +164,7 @@ def tree_intersect(
         (points[triangle.left], points[triangle.current], points[triangle.right])
     ).bbox()
 
-    with mutex:
-        candidates = tree.intersection(bounding_rect, objects=True)
+    candidates = tree.intersection(bounding_rect, objects=True)
 
     for candidate_item in candidates:
         candidate: Line = candidate_item.object  # type: ignore
