6fd762819059d55b032c2d790863af69703d7868
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-07-08 00:37:47 +0100

    refactor: replace orientation with geo kernel

--- src/convex_hull.rs
+++ src/convex_hull.rs
@@ -1,5 +1,4 @@
-use geo::{Coord, CoordsIter, GeoNum, Orientation, Point, Polygon};
-use std::cmp::Ordering;
+use geo::{CoordsIter, GeoNum, Kernel, Orientation, Polygon};
 use std::collections::VecDeque;
 use std::ops::Sub;
 
@@ -18,18 +17,6 @@ impl<T> SecondIndex<T> for VecDeque<T> {
     }
 }
 
-fn orientation<T>(x: Coord<T>, y: Coord<T>, z: Coord<T>) -> Orientation
-where
-    T: GeoNum,
-{
-    let (x, y, z) = (Point::from(x), Point::from(y), Point::from(z));
-    match x.cross_prod(y, z).total_cmp(&T::zero()) {
-        Ordering::Less => Orientation::CounterClockwise,
-        Ordering::Equal => Orientation::Collinear,
-        Ordering::Greater => Orientation::Clockwise,
-    }
-}
-
 pub fn melkman_indices<T>(poly: &Polygon<T>) -> Vec<usize>
 where
     T: GeoNum,
@@ -41,21 +28,21 @@ where
 
     for (index, v) in poly_iter {
         if matches!(
-            orientation(v, hull.back().unwrap().1, hull.back_less().unwrap().1),
+            T::Ker::orient2d(v, hull.back().unwrap().1, hull.back_less().unwrap().1),
             Orientation::CounterClockwise
         ) || matches!(
-            orientation(v, hull.front().unwrap().1, hull.front_less().unwrap().1),
+            T::Ker::orient2d(v, hull.front().unwrap().1, hull.front_less().unwrap().1),
             Orientation::Clockwise
         ) {
             while let Orientation::CounterClockwise =
-                orientation(v, hull.back().unwrap().1, hull.back_less().unwrap().1)
+                T::Ker::orient2d(v, hull.back().unwrap().1, hull.back_less().unwrap().1)
             {
                 hull.pop_back();
             }
             hull.push_back((index, v));
 
             while let Orientation::Clockwise =
-                orientation(v, hull.front().unwrap().1, hull.front_less().unwrap().1)
+                T::Ker::orient2d(v, hull.front().unwrap().1, hull.front_less().unwrap().1)
             {
                 hull.pop_front();
             }
