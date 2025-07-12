4b5c802df5e326fd13543371978c1c20de2e289b
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-07-12 18:22:07 +0100

    feat: add plotting script

---
+++ scripts/plot_poly.py
@@ -0,0 +1,22 @@
+"""Plot a polygon and its reduction."""
+
+import pickle
+
+import matplotlib.pyplot as plt
+
+from polyshell import reduce_polygon
+
+if __name__ == "__main__":
+    with open("./tests/data/ionian_polygon_points.pkl", "rb") as f:
+        original_polygon = pickle.load(f)
+
+    reduced_polygon = reduce_polygon(original_polygon, epsilon=1e-6)
+
+    # Extract data
+    x_orig, y_orig = zip(*original_polygon)
+    x_reduced, y_reduced = zip(*reduced_polygon)
+
+    # Plot original and reduced polygons
+    plt.plot(x_orig, y_orig, "b-")
+    plt.plot(x_reduced, y_reduced, "r-")
+    plt.show()
