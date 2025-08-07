4b7cfae853d8d794acb9e035ac13706d65a5b89d
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-08-07 19:23:28 +0100

    test: integrate charshape with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 """Fixtures and test cases."""
 
-from pytest_cases import fixture  # type: ignore
+from pytest_cases import fixture, parametrize_with_cases  # type: ignore
 
 from polyshell import reduce_polygon
 from polyshell.geometry import Polygon
+from polyshell.reducer import ReductionMethods
 
 
 @fixture(scope="class")
-def simplified(polygon: Polygon) -> Polygon:
-    return reduce_polygon(polygon, epsilon=1e-6)
+@parametrize_with_cases("method", cases=".method_cases", scope="class")
+def simplified(polygon: Polygon, method: ReductionMethods) -> Polygon:
+    return reduce_polygon(polygon, epsilon=1e-6, method=method)
