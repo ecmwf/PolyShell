2c87f7df600d5cdfa5da59a2ac633ac2494463f8
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-08-08 11:23:27 +0100

    temp: add adaptive fit

--- src/polyshell/cli/plot.py
+++ src/polyshell/cli/plot.py
@@ -1,11 +1,60 @@
 from pathlib import Path
 
+import numpy as np
+import numpy.typing as npt
 import typer
 from matplotlib import pyplot as plt
+from scipy.stats import linregress
 
+from polyshell import reduce_polygon
 from polyshell.cli.utils import load_from_path
+from polyshell.convex_hull import melkman, melkman_indices
 
 app = typer.Typer()
+
+
+def adaptive_step(
+    eps: npt.NDArray[np.float64], length: npt.NDArray[np.int64], min_length: int
+) -> any:
+    """Predict an adaptive step size based on past data."""
+    fit = linregress(eps, 1 / (length - min_length))
+
+    return fit
+
+
+# TODO: Write a func which selects and adaptive epsilon based on reduction rate
+
+
+@app.command()
+def loss_curve(input_path: Path, startup_step: float = 1e-4, startup_count: int = 10):
+    """Plot the loss curve."""
+    poly = load_from_path(input_path)
+    min_len = len(melkman(poly))
+
+    # Start-up
+    startup_eps = startup_step * np.arange(startup_count)
+    startup_len: list[int] = []
+    for eps in startup_eps:
+        poly = reduce_polygon(poly, eps)
+        startup_len.append(len(poly))
+    startup_len = np.array(startup_len)
+    fit = adaptive_step(startup_eps, startup_len, min_len)
+
+    # Adaptive step
+    pred_len = np.linspace(min_len, len(poly), 100)[1::][::-1]
+    eps_vals = (1 / (pred_len - min_len) - fit.intercept) / fit.slope
+
+    poly_len: list[int] = []
+    for eps in eps_vals:
+        poly = reduce_polygon(poly, eps)
+        poly_len.append(len(poly))
+
+    plt.plot(eps_vals, poly_len, label="Loss")
+    plt.plot(eps_vals, pred_len, label="Predicted")
+    plt.xlabel("Epsilon")
+    plt.ylabel("Count")
+    plt.legend()
+    plt.show()
 
 
 @app.callback(invoke_without_command=True)
@@ -13,5 +62,5 @@ def plot(input_path: Path):
     """Plot a polygon from an input file."""
     poly = load_from_path(input_path)
     x, y = zip(*poly)
-    plt.plot(x, y)
-    plt.show()
+    # plt.plot(x, y)
+    # plt.show()
--- src/polyshell/cli/utils/file_formats.py
+++ src/polyshell/cli/utils/file_formats.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 from polyshell.geometry import Polygon
 
-supported_formats = {".pkl": PickleFileFormat}
-
 
 def load_from_path(path: Path) -> Polygon:
     if path.suffix in supported_formats:
@@ -21,23 +19,28 @@ def save_to_path(polygon: Polygon, output_path: Path):
         raise FileNotFoundError("Unsupported file format")
 
 
-class FileFormat(ABC):
+class FileFormat:
     """Base class for file formats with can load and store polygons."""
 
-    @abstractmethod
-    def load(self, path: Path) -> Polygon:
-        pass
+    @staticmethod
+    def load(path: Path) -> Polygon:
+        raise NotImplementedError()
 
-    @abstractmethod
-    def save(self, data: Polygon, path: Path):
-        pass
+    @staticmethod
+    def save(data: Polygon, path: Path):
+        raise NotImplementedError()
 
 
 class PickleFileFormat(FileFormat):
-    def load(self, path: Path) -> Polygon:
+    @staticmethod
+    def load(path: Path) -> Polygon:
         with open(path, "rb") as f:
             return Polygon.from_array(pickle.load(f))
 
-    def save(self, data: Polygon, path: Path):
+    @staticmethod
+    def save(data: Polygon, path: Path):
         with open(path, "wb") as f:
             pickle.dump(data.to_array(), f)
+
+
+supported_formats = {".pkl": PickleFileFormat}
