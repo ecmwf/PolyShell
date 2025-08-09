7300dd98100a445a999ff61d64befe09000222c4
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-08-09 19:55:57 +0100

    refactor: add checkpointing for Visvalingam-Whyatt

--- src/polyshell/reducer/vw/vw.py
+++ src/polyshell/reducer/vw/vw.py
@@ -35,164 +35,155 @@ class VWScore:
         self.sort_index = self.score
 
 
-def data_stream(geoms: Iterable[Geometry]):
-    for item in geoms:
-        yield ID, item.bbox(), item
+class VWState:
+    def __init__(self, orig: LineString, loss_fn: LossFunction):
+        self.orig = orig
+        self.loss_fn = loss_fn
 
+        self.max = len(orig)
+        self.tree = Rtree(data_stream(orig.lines()))
+        self.adjacent = [(i - 1, i + 1) for i in range(len(orig))]
+        self.pq = [
+            VWScore(
+                score,
+                current=i + 1,
+                left=i,
+                right=i + 2,
+            )
+            for i, triangle in enumerate(orig.triangles())
+            if (score := loss_fn(triangle)) >= 0
+        ]
+        heapq.heapify(self.pq)
+        self.loss = 0.0
 
-def worker_wraps(epsilon: float, loss_fn: LossFunction):
-    def worker(ls: LineString) -> LineString:
-        return vw_preserve(ls, epsilon, loss_fn)
+    def into(self) -> LineString:
+        return LineString(
+            [point for point, adj in zip(self.orig, self.adjacent) if adj != (0, 0)]
+        )
 
-    return worker
+    def reduce(self, epsilon: float) -> None:
+        while len(self.pq):
+            smallest = heapq.heappop(self.pq)
+
+            if smallest.score > epsilon:
+                # smallest is not removed so return to the heap
+                heapq.heappush(self.pq, smallest)
+                break
+
+            # Check if the score is invalidated
+            left, right = self.adjacent[smallest.current]
+            if left != smallest.left or right != smallest.right:
+                continue
+
+            # Check for self-intersection
+            if self.tree_intersect(smallest):
+                # Skip this point
+                continue
+
+            # Update adjacency list
+            ll, _ = self.adjacent[left]
+            _, rr = self.adjacent[right]
+            self.adjacent[left] = (ll, right)
+            self.adjacent[right] = (left, rr)
+            self.adjacent[smallest.current] = (0, 0)
+
+            # Reconnect vertices
+            left_point = self.orig[left]
+            right_point = self.orig[right]
+            new_line = Line((left_point, right_point))
+            self.tree.insert(ID, new_line.bbox(), new_line)
+
+            # Update adjacent triangles
+            self.recompute_triangles(ll, left, right, rr)
+
+            # Update loss
+            self.loss += smallest.score
+
+    def tree_intersect(self, triangle: VWScore) -> bool:
+        """Check if removal of a triangle causes a self-intersection."""
+        new_segment_start = self.orig[triangle.left]
+        new_segment_end = self.orig[triangle.right]
+
+        new_segment = Line((new_segment_start, new_segment_end))
+
+        bounding_rect = Triangle(
+            (
+                self.orig[triangle.left],
+                self.orig[triangle.current],
+                self.orig[triangle.right],
+            )
+        ).bbox()
+
+        candidates = self.tree.intersection(bounding_rect, objects=True)
+
+        for candidate_item in candidates:
+            candidate: Line = candidate_item.object  # type: ignore
+            candidate_start, candidate_end = candidate
+            if (
+                candidate_start != new_segment_start
+                and candidate_start != new_segment_end
+                and candidate_end != new_segment_start
+                and candidate_end != new_segment_end
+                and new_segment.intersects(candidate)
+            ):
+                return True
+
+        else:
+            return False
+
+    def recompute_triangles(
+        self,
+        ll: int,
+        left: int,
+        right: int,
+        rr: int,
+    ):
+        choices = [(ll, left, right), (left, right, rr)]
+        for ai, current_point, bi in choices:
+            if not (0 <= ai < self.max and 0 <= bi < self.max):
+                continue  # out of bounds
+
+            score = self.loss_fn(
+                Triangle((self.orig[ai], self.orig[current_point], self.orig[bi]))
+            )
+            if score < 0:
+                continue  # do not push negative areas
+
+            v = VWScore(
+                score,
+                current=current_point,
+                left=ai,
+                right=bi,
+            )
+            heapq.heappush(self.pq, v)
 
 
 def reduce_polygon_vw(
-    polygon: Polygon,
-    epsilon: float,
-    loss_fn: LossFunction,
-    max_workers: Optional[int] = None,
+        polygon: Polygon,
+        epsilon: float,
+        loss_fn: LossFunction,
+        max_workers: Optional[int] = None,
 ) -> Polygon:
     """Reduce a polygon while retaining coverage."""
     vertices = melkman_indices(polygon)
     segments = [
         polygon[start : end + 1] for start, end in zip(vertices[:-1], vertices[1:])
     ]
+    vw_states = [VWState(ls, loss_fn) for ls in segments]
 
     with ThreadPoolExecutor(max_workers) as tpe:
-        reduced_segments = tpe.map(worker_wraps(epsilon, loss_fn), segments)
+        tpe.map(worker_wraps(epsilon), vw_states)
 
-    return Polygon.merge(reduced_segments)
-
-
-def vw_preserve(
-    polygon_points: LineString, epsilon: float, loss_fn: LossFunction
-) -> LineString:
-    """Visvalingam-Whyatt line reduction algorithm adapted to prevent crossings."""
-    if len(polygon_points) < 3 or epsilon <= 0:
-        return polygon_points
-
-    max_points = len(polygon_points)
-
-    tree = Rtree(data_stream(polygon_points.lines()))
-
-    adjacent = [(i - 1, i + 1) for i in range(max_points)]
-
-    pq = [
-        VWScore(
-            score,
-            current=i + 1,
-            left=i,
-            right=i + 2,
-        )
-        for i, triangle in enumerate(polygon_points.triangles())
-        if (score := loss_fn(triangle)) >= 0
-    ]
-    heapq.heapify(pq)
-
-    loss = 0
-    while len(pq):
-        smallest = heapq.heappop(pq)
-
-        if smallest.score > epsilon:
-            break
-
-        # Check if the score is invalidated
-        left, right = adjacent[smallest.current]
-        if left != smallest.left or right != smallest.right:
-            continue
-
-        # Check for self-intersection
-        if tree_intersect(tree, smallest, polygon_points):
-            # Skip this point
-            continue
-
-        # Update adjacency list
-        ll, _ = adjacent[left]
-        _, rr = adjacent[right]
-        adjacent[left] = (ll, right)
-        adjacent[right] = (left, rr)
-        adjacent[smallest.current] = (0, 0)
-
-        # Reconnect vertices
-        left_point = polygon_points[left]
-        right_point = polygon_points[right]
-        new_line = Line((left_point, right_point))
-        tree.insert(ID, new_line.bbox(), new_line)
-
-        # Update adjacent triangles
-        recompute_triangles(
-            polygon_points,
-            pq,
-            ll,
-            left,
-            right,
-            rr,
-            max_points,
-            loss_fn,
-        )
-
-        # Update loss
-        loss += smallest.score
-
-    # Filter out any deleted points
-    return LineString(
-        [point for point, adj in zip(polygon_points, adjacent) if adj != (0, 0)]
-    )
+    return Polygon.merge([state.into() for state in vw_states])
 
 
-def recompute_triangles(
-    points: LineString,
-    pq: list[VWScore],
-    ll: int,
-    left: int,
-    right: int,
-    rr: int,
-    max: int,
-    loss_fn: LossFunction,
-):
-    choices = [(ll, left, right), (left, right, rr)]
-    for ai, current_point, bi in choices:
-        if not (0 <= ai < max and 0 <= bi < max):
-            continue  # out of bounds
-
-        score = loss_fn(Triangle((points[ai], points[current_point], points[bi])))
-        if score < 0:
-            continue  # do not push negative areas
-
-        v = VWScore(
-            score,
-            current=current_point,
-            left=ai,
-            right=bi,
-        )
-        heapq.heappush(pq, v)
+def data_stream(geoms: Iterable[Geometry]):
+    for item in geoms:
+        yield ID, item.bbox(), item
 
 
-def tree_intersect(tree: Rtree, triangle: VWScore, points: LineString) -> bool:
-    """Check if removal of a triangle causes a self-intersection."""
-    new_segment_start = points[triangle.left]
-    new_segment_end = points[triangle.right]
+def worker_wraps(epsilon: float):
+    def worker(state: VWState):
+        state.reduce(epsilon)
 
-    new_segment = Line((new_segment_start, new_segment_end))
-
-    bounding_rect = Triangle(
-        (points[triangle.left], points[triangle.current], points[triangle.right])
-    ).bbox()
-
-    candidates = tree.intersection(bounding_rect, objects=True)
-
-    for candidate_item in candidates:
-        candidate: Line = candidate_item.object  # type: ignore
-        candidate_start, candidate_end = candidate
-        if (
-            candidate_start != new_segment_start
-            and candidate_start != new_segment_end
-            and candidate_end != new_segment_start
-            and candidate_end != new_segment_end
-            and new_segment.intersects(candidate)
-        ):
-            return True
-
-    else:
-        return False
+    return worker
