e877ed9b4f67d91ddd4affc9791d918e903c4356
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-08-13 14:42:52 +0100

    fixup

--- src/polyshell/cli/plot.py
+++ src/polyshell/cli/plot.py
@@ -14,10 +14,10 @@ app = typer.Typer()
 
 
 def adaptive_step(
-    eps: npt.NDArray[np.float64], length: npt.NDArray[np.int64], min_length: int
+    eps: npt.NDArray[np.float64], length: npt.NDArray[np.int64], min_length: int, gamma: float = -1.0
 ) -> any:
     """Predict an adaptive step size based on past data."""
-    fit = linregress(eps, 1 / (length - min_length))
+    fit = linregress(eps, (length - min_length)**gamma)
 
     return fit
 
@@ -26,43 +26,34 @@ def adaptive_step(
 
 
 @app.command()
-def loss_curve(input_path: Path, startup_step: float = 1e-4, startup_count: int = 100):
+def loss_curve(input_path: Path, startup_step: float = 1e-6, startup_count: int = 100):
     """Plot the loss curve."""
     poly = load_from_path(input_path)
     min_len = len(melkman(poly))
+    print(f"{min_len} / {len(poly)}")
+
+    gamma = -1.65
 
     # Start-up
-    startup_eps = startup_step * np.arange(startup_count)
+    startup_eps = startup_step * np.arange(1, startup_count + 1)
     startup_len: list[int] = []
     for eps in startup_eps:
         poly = reduce_polygon(poly, eps)
         startup_len.append(len(poly))
     startup_len = np.array(startup_len)
-    fit = adaptive_step(startup_eps, startup_len, min_len)
+    fit = adaptive_step(startup_eps, startup_len, min_len, gamma=gamma)
 
-    # plt.plot(startup_eps, startup_len)
+    # plt.plot(startup_eps, (startup_len - min_len)**gamma)
     # plt.xlabel("Epsilon")
-    # plt.ylabel("No. points")
+    # plt.ylabel(f"(No. points - No. hull points)^{gamma:.2f}")
     # plt.show()
     # assert False
 
-    gamma = 1.0
-    plt.plot(startup_eps, 1 / (startup_len - min_len)**gamma)
-    plt.xlabel("Epsilon")
-    plt.ylabel(f"1 / (No. points - No. hull points)^{gamma:.2f}")
-    plt.show()
-    assert False
-
-    # plt.plot(startup_eps, np.log(startup_len - min_len))
-    # plt.xlabel("Epsilon")
-    # plt.ylabel("log(No. points - No. hull points)")
-    # plt.show()
-    # assert False
-
+    # a eps + b = (L - l)**gamma
 
     # Adaptive step
     pred_len = np.linspace(min_len, len(poly), 100)[1::][::-1]
-    eps_vals = (1 / (pred_len - min_len) - fit.intercept) / fit.slope
+    eps_vals = ((pred_len - min_len)**gamma - fit.intercept) / fit.slope
 
     poly_len: list[int] = []
     for eps in eps_vals:
