d5610a59e32022505d9ac26dc99e1bdb30ac4d56
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-08-22 12:49:32 +0100

    fix: improve RDP reduction rate

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -2,5 +2,6 @@ pub mod charshape;
 mod line_intersect;
 pub mod melkman;
 pub mod rdp;
+mod visibility;
 pub mod vw;
 pub mod vw_convex;
--- src/algorithms/rdp.rs
+++ src/algorithms/rdp.rs
@@ -1,13 +1,11 @@
-use crate::extensions::ord_triangles::OrdTriangle;
+use crate::algorithms::visibility::visiblity_polygon;
 use crate::extensions::segments::{FromSegments, HullSegments};
-use geo::{Area, Coord, GeoFloat, Intersects, Line, LineString, Polygon};
+use geo::{Coord, Distance, Euclidean, GeoFloat, Line, LineString, Polygon};
 use rayon::prelude::*;
-use rstar::primitives::CachedEnvelope;
-use rstar::{RTree, RTreeNum, RTreeObject};
 
-fn rdp_preserve<T>(ls: &[Coord<T>], eps: T, tree: &RTree<CachedEnvelope<Line<T>>>) -> Vec<Coord<T>>
+fn rdp_preserve<T>(ls: &[Coord<T>], eps: T) -> Vec<Coord<T>>
 where
-    T: GeoFloat + RTreeNum + Send + Sync,
+    T: GeoFloat + Send + Sync,
 {
     let (first, last) = match ls {
         [] => return vec![],
@@ -16,35 +14,45 @@ where
         &[first, .., last] => (first, last),
     };
 
-    let (farthest_index, farthest_distance) = ls
-        .iter()
-        .enumerate()
-        .take(ls.len() - 1)
+    let visible = visiblity_polygon(ls);
+    let chord = Line::new(first, last);
+
+    let split_index = visible
+        .into_iter()
         .skip(1)
-        .map(|(index, &coord)| (index, OrdTriangle(first, coord, last).signed_area()))
         .fold(
             (0usize, T::zero()),
-            |(farthest_index, farthest_distance), (index, distance)| {
-                if farthest_distance < T::zero() {
-                    if distance < farthest_distance {
-                        return (index, distance);
-                    }
-                } else if distance.abs() >= farthest_distance.abs() {
-                    return (index, distance);
-                };
-                (farthest_index, farthest_distance)
+            |(farthest_index, farthest_distance), (index, coord)| {
+                let distance = Euclidean.distance(coord, &chord);
+                if distance < farthest_distance {
+                    (farthest_index, farthest_distance)
+                } else {
+                    (index, distance)
+                }
             },
-        );
+        )
+        .0;
 
-    let first_last_line = CachedEnvelope::new(Line::new(first, last));
+    if split_index == 0 || split_index == ls.len() - 1 {
+        println!("Failed to reduce. Skipping.");
+        return vec![first, last];
+    }
 
-    if farthest_distance < T::zero()
-        || farthest_distance > eps
-        || tree_intersect(first_last_line, tree)
-    {
+    let farthest_distance = ls.iter().map(|&v| Euclidean.distance(v, &chord)).fold(
+        T::zero(),
+        |farthest_distance, distance| {
+            if distance > farthest_distance {
+                distance
+            } else {
+                farthest_distance
+            }
+        },
+    );
+
+    if farthest_distance > eps {
         let (mut left, right) = rayon::join(
-            || rdp_preserve(&ls[..=farthest_index], eps, tree),
-            || rdp_preserve(&ls[farthest_index..], eps, tree),
+            || rdp_preserve(&ls[..=split_index], eps),
+            || rdp_preserve(&ls[split_index..], eps),
         );
 
         left.pop();
@@ -56,43 +64,22 @@ where
     vec![first, last]
 }
 
-fn tree_intersect<T>(line: CachedEnvelope<Line<T>>, tree: &RTree<CachedEnvelope<Line<T>>>) -> bool
-where
-    T: GeoFloat + RTreeNum,
-{
-    let (start, end) = line.points();
-    let bounding_rect = line.envelope();
-
-    tree.locate_in_envelope_intersecting(&bounding_rect)
-        .any(|candidate| {
-            let (candidate_start, candidate_end) = candidate.points();
-            candidate_start.0 != start.0
-                && candidate_start.0 != end.0
-                && candidate_end.0 != start.0
-                && candidate_end.0 != end.0
-                && line.intersects(&**candidate)
-        })
-}
-
 pub trait SimplifyRDP<T, Epsilon = T> {
     fn simplify_rdp(&self, eps: Epsilon) -> Self;
 }
 
 impl<T> SimplifyRDP<T> for LineString<T>
 where
-    T: GeoFloat + RTreeNum + Send + Sync,
+    T: GeoFloat + Send + Sync,
 {
     fn simplify_rdp(&self, eps: T) -> Self {
-        let tree: RTree<CachedEnvelope<_>> =
-            RTree::bulk_load(self.lines().map(CachedEnvelope::new).collect::<Vec<_>>());
-
-        LineString::new(rdp_preserve(&self.0, eps, &tree))
+        LineString::new(rdp_preserve(&self.0, eps))
     }
 }
 
 impl<T> SimplifyRDP<T> for Polygon<T>
 where
-    T: GeoFloat + RTreeNum + Send + Sync,
+    T: GeoFloat + Send + Sync,
 {
     fn simplify_rdp(&self, eps: T) -> Self {
         let reduced_segments = self
