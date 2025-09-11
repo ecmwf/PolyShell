32fb69921a6eaf0aff740c27c357f5c014c62db5
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-09-11 11:58:10 +0100

    test: update tests to new API

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 """Fixtures and test cases."""
 
-from typing import Callable
-
 from pytest_cases import fixture, parametrize_with_cases  # type: ignore
+
+from polyshell import ReductionMethod, ReductionMode, reduce_polygon
 
 
 @fixture(scope="class")
 @parametrize_with_cases("method", cases=".method_cases", scope="class")
 def simplified(
-    polygon: list[tuple[float, float]], method: Callable
+    polygon: list[tuple[float, float]], method: ReductionMethod
 ) -> list[tuple[float, float]]:
-    return method(polygon, eps=1e-6)
+    return reduce_polygon(polygon, ReductionMode.EPSILON, 1e-6, method)
