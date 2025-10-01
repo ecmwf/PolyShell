783186b17a9969b9cedd6f1bc06f7c1535fd78d1
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-10-01 14:11:08 +0100

    feat: add unchecked algorithms

--- src/lib.rs
+++ src/lib.rs
@@ -30,9 +30,41 @@ fn reduce_polygon_vw(orig: Vec<[f64; 2]>, eps: f64, len: usize) -> PyResult<Vec<
 }
 
 #[pyfunction]
+fn reduce_polygon_vw_unchecked(
+    orig: Vec<[f64; 2]>,
+    eps: f64,
+    len: usize,
+) -> PyResult<Vec<(f64, f64)>> {
+    // Instantiate a Polygon from a Vec of coordinates
+    let polygon = Polygon::new(orig.into(), vec![]);
+
+    // Reduce and extract coordinates
+    let (exterior, _) = polygon.simplify_vw(eps, len).into_inner();
+    let coords = exterior.into_iter().map(|c| c.x_y()).collect::<Vec<_>>();
+
+    Ok(coords)
+}
+
+#[pyfunction]
 fn reduce_polygon_char(orig: Vec<[f64; 2]>, eps: f64, len: usize) -> PyResult<Vec<(f64, f64)>> {
     // Instantiate a Polygon from a Vec of coordinates
     let polygon = Polygon::new(orig.into(), vec![]).validate()?;
+
+    // Reduce and extract coordinates
+    let (exterior, _) = polygon.simplify_charshape(eps, len).into_inner();
+    let coords = exterior.into_iter().map(|c| c.x_y()).collect::<Vec<_>>();
+
+    Ok(coords)
+}
+
+#[pyfunction]
+fn reduce_polygon_char_unchecked(
+    orig: Vec<[f64; 2]>,
+    eps: f64,
+    len: usize,
+) -> PyResult<Vec<(f64, f64)>> {
+    // Instantiate a Polygon from a Vec of coordinates
+    let polygon = Polygon::new(orig.into(), vec![]);
 
     // Reduce and extract coordinates
     let (exterior, _) = polygon.simplify_charshape(eps, len).into_inner();
@@ -54,10 +86,27 @@ fn reduce_polygon_rdp(orig: Vec<[f64; 2]>, eps: f64) -> PyResult<Vec<(f64, f64)>
     Ok(coords)
 }
 
+#[pyfunction]
+fn reduce_polygon_rdp_unchecked(orig: Vec<[f64; 2]>, eps: f64) -> PyResult<Vec<(f64, f64)>> {
+    // Instantiate a Polygon from a Vec of coordinates
+    let polygon = Polygon::new(orig.into(), vec![]);
+
+    // Reduce and extract coordinates
+    let (exterior, _) = polygon.simplify_rdp(eps).into_inner();
+    let coords = exterior.into_iter().map(|c| c.x_y()).collect::<Vec<_>>();
+
+    Ok(coords)
+}
+
 #[pymodule]
 fn _polyshell(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(reduce_polygon_vw, m)?)?;
     m.add_function(wrap_pyfunction!(reduce_polygon_char, m)?)?;
     m.add_function(wrap_pyfunction!(reduce_polygon_rdp, m)?)?;
+
+    m.add_function(wrap_pyfunction!(reduce_polygon_vw_unchecked, m)?)?;
+    m.add_function(wrap_pyfunction!(reduce_polygon_char_unchecked, m)?)?;
+    m.add_function(wrap_pyfunction!(reduce_polygon_rdp_unchecked, m)?)?;
+
     Ok(())
 }
