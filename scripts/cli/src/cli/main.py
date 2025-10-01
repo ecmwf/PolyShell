61867a872f0605d61ae4e29cbdb40e91c2fcccb0
Niall Oswald <niall.oswald@hotmail.co.uk>
2025-10-01 14:04:51 +0100

    chore: sort imports

--- scripts/cli/src/cli/main.py
+++ scripts/cli/src/cli/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import typer
 from matplotlib import pyplot as plt
+
 from polyshell import ReductionMethod, ReductionMode, reduce_polygon
 
 app = typer.Typer(no_args_is_help=True)
